ebfa4378791950d71918c7bb85ba0030fd4d533e
陈湾 <chenwan@xmfunny.com>
2021-10-28 19:54:20 +0800

    <refactor> 修改触发事件的方式——现在不需要反射由IL生成的代码

--- DirtyScriptsCompilationTool/Editor/DirtyScriptsCompilationTool.cs
+++ DirtyScriptsCompilationTool/Editor/DirtyScriptsCompilationTool.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Linq;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.Compilation;
@@ -40,7 +40,7 @@ namespace FinalFrame.EditorTool {
             EditorCompilationFileWatcher.Init(Application.dataPath);
             EditorCompilationFileWatcher.OnScriptDirtyHandle = (path) => dirtyHandleQueue.Enqueue(() => OnScriptDirty(path));
 
-            UnityEngine.Debug.Log("DirtyScriptsCompilationTool 初始化完成");
+            Debug.Log("DirtyScriptsCompilationTool 初始化完成");
 
         }
 
@@ -74,7 +74,7 @@ namespace FinalFrame.EditorTool {
                     if (asmCacheModel == null) {
                         asmCacheModel = ScriptableObject.CreateInstance<EditorCompilationAsmCacheModelSo>();
                     } else {
-                        // UnityEngine.Debug.Log("找到已存在 So");
+                        // Debug.Log("找到已存在 So");
                     }
                     asmCacheModel.hideFlags = HideFlags.DontSave;
                     asmCacheModel.Init();
@@ -88,30 +88,18 @@ namespace FinalFrame.EditorTool {
         [MenuItem("Tools/DirtyScriptsCompilation/Compile %_T")]
         static void CompileDirtyScripts() {
 
-            Stopwatch stopwatch = new Stopwatch();
+            var stopwatch = new System.Diagnostics.Stopwatch();
             stopwatch.Start();
 
-            Type compilationPipeline = typeof(CompilationPipeline);
+            // 找到触发编译完成的方法
+            // 以及 out 方法参数 1
+            MethodInfo invokeMethod = FindInvokerInEditorCompilation(out object invoker);
 
-            // 找到事件
-            EventInfo eventInfo = compilationPipeline.GetEvent("assemblyCompilationFinished", BindingFlags.Static | BindingFlags.Public);
-
-            // 根据事件找到触发方法
-            Type handlerType = eventInfo.EventHandlerType;
-            MethodInfo invokeMethod = handlerType.GetMethod("Invoke");
-
-            // 找到触发方法所需要的实例
-            object invoker = null;
-            MemberInfo[] memberInfos = compilationPipeline.GetMembers(BindingFlags.Static | BindingFlags.NonPublic);
-            foreach (var mem in memberInfos) {
-                if (mem.Name == "assemblyCompilationFinished") {
-                    FieldInfo fieldInfo = mem as FieldInfo;
-                    invoker = fieldInfo.GetValue(null);
-                }
-            }
+            // 方法参数 2
+            Type type = System.Reflection.Assembly.Load("UnityEditor").GetType("UnityEditor.Scripting.Compilers.CompilerMessage");
+            var list = Activator.CreateInstance(typeof(List<>).MakeGenericType(type));
 
             var model = GetOrCreateAsmCacheModelSo();
-
             List<string> dirties = model.GetDirtyScripts();
 
             // 编译
@@ -121,9 +109,9 @@ namespace FinalFrame.EditorTool {
 
                 // 编译 asm
                 EditorUtility.CompileCSharp(asm.sourceFiles, asm.allReferences, asm.defines, asm.outputPath);
-                UnityEngine.Debug.Log($"编译 asm: {asm.name}, sourceFilesCount: {asm.sourceFiles.Length.ToString()}, refsCount: {asm.allReferences.Length.ToString()}, outPath: {asm.outputPath}");
+                Debug.Log($"编译 asm: {asm.name}, sourceFilesCount: {asm.sourceFiles.Length.ToString()}, refsCount: {asm.allReferences.Length.ToString()}, outPath: {asm.outputPath}");
 
-                invokeMethod.Invoke(invoker, new object[] { asm.outputPath, new CompilerMessage[0] });
+                invokeMethod.Invoke(invoker, new object[] { asm.outputPath, list });
 
             }
 
@@ -131,14 +119,53 @@ namespace FinalFrame.EditorTool {
 
             // 无脚本
             if (dirties.Count == 0) {
-                UnityEngine.Debug.Log($"不存在脏脚本, 无需编译, 耗时: {stopwatch.ElapsedMilliseconds.ToString()}");
+                Debug.Log($"不存在脏脚本, 无需编译, 耗时: {stopwatch.ElapsedMilliseconds.ToString()}");
             } else {
-                UnityEngine.Debug.Log($"合计编译 DirtyScripts 数量: {dirties.Count}, 耗时: {stopwatch.ElapsedMilliseconds.ToString()}");
+                Debug.Log($"合计编译 DirtyScripts 数量: {dirties.Count}, 耗时: {stopwatch.ElapsedMilliseconds.ToString()}");
             }
 
             // 清空 DirtyScripts
             dirtyHandleQueue.Clear();
             model.CleanDirtyScripts();
+
+        }
+
+        static MethodInfo FindInvoker(out object invoker) {
+
+            Type compilationPipeline = typeof(CompilationPipeline);
+
+            // 找到事件
+            EventInfo eventInfo = compilationPipeline.GetEvent("assemblyCompilationFinished", BindingFlags.Static | BindingFlags.Public);
+            
+            // 根据事件找到触发方法
+            Type handlerType = eventInfo.EventHandlerType;
+            MethodInfo invokeMethod = handlerType.GetMethod("Invoke");
+
+            // 找到触发方法所需要的实例
+            FieldInfo fi = compilationPipeline.GetField("assemblyCompilationFinished", BindingFlags.NonPublic | BindingFlags.Static);
+            invoker = fi.GetValue(null);
+            Debug.Log(invoker);
+
+            return invokeMethod;
+
+        }
+
+        static MethodInfo FindInvokerInEditorCompilation(out object invoker) {
+
+            Type[] allType = System.Reflection.Assembly.Load("UnityEditor").GetTypes();
+            Type ecInterface = null;
+            foreach (var type in allType) {
+                if (type.Name.Contains("EditorCompilationInterface")) {
+                    ecInterface = type;
+                }
+            }
+            object ec = ecInterface.GetProperty("Instance", BindingFlags.Static | BindingFlags.Public).GetValue(null);
+
+            MethodInfo invokeMethod = ec.GetType().GetMethod("InvokeAssemblyCompilationFinished", BindingFlags.Public | BindingFlags.Instance);
+
+            invoker = ec;
+
+            return invokeMethod;
 
         }
 
